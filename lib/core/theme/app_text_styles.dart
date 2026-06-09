c9b2acb14a0d4876451f5d4d4955b73156131310
DESKTOP-RTT4B09\Thanh Dao <tienthanhdao003@gmail.com>
2026-06-09 22:51:25 +0700

    add design system

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  AppTextStyles._();
+  static const heading1 = TextStyle(fontSize: 32, fontWeight: FontWeight.bold);
+
+  static const heading2 = TextStyle(fontSize: 24, fontWeight: FontWeight.w700);
+
+  static const bodyLarge = TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
+
+  static const bodyMedium = TextStyle(fontSize: 14, fontWeight: FontWeight.w400);
+}
--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../../../core/theme/app_colors.dart';
+//import '../../../../core/theme/app_colors.dart';
+import 'package:sneakerx/core/theme/app_text_styles.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -8,7 +9,7 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('SneakerX', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: AppColors.primary)), 
+        child: Text('SneakerX', style: AppTextStyles.heading1,), 
       ),
     );
   }
